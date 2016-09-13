2fb78c9959275a1449940d2ca73601d611233c0a
Kris Szafranski <kdszafranski@gmail.com>
2016-09-13 11:56:42 -0500

    updated for demo

--- server/strategies/user_sql.js
+++ server/strategies/user_sql.js
@@ -1,10 +1,4 @@
-/*
-CREATE TABLE users (
- id SERIAL PRIMARY KEY,
- username VARCHAR(100) NOT NULL UNIQUE,
- password VARCHAR(120) NOT NULL
-);
-*/
+
 
 var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
